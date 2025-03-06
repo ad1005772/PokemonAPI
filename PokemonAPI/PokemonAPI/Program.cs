0eabc7847c7232fc0a1dec992f81b9597eabc8ed
ad1005772 <ad1005772@otc.edu>
2025-03-06 10:12:11 -0600

    WorkingOnStep2
    In the process of setting up the Pokemon class.

---
+++ PokemonAPI/PokemonAPI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static async Task Main(string[] arg)
+    {
+        string apiURL = "https://pokeapi.co/api/v2/";
+        using HttpClient client = new HttpClient();
+        HttpResponseMessage response = await client.GetAsync(apiURL);
+        response.EnsureSuccessStatusCode();
+        string content = await response.Content.ReadAsStringAsync();
+        string data = JsonSerializer.Deserialize<string>(content);
+
+    }
+}
